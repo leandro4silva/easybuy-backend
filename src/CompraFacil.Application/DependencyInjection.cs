fcb58f86c9864a5b14835557f53bb079afbacf5e
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-11-17 01:13:04 -0300

    feat: order controller

---
+++ src/CompraFacil.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using CompraFacil.Application.Handlers.v1.Order.CreateOrder;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompraFacil.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        #region MediatR
+        
+        services.AddMediatR(typeof(CreateOrderHandler));
+
+        #endregion
+
+        return services;
+    }
+}
